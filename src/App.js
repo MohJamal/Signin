ae6c8fef50b1436067bc99f96ea011b82ed83ba3
MohJamal <mohjamalalkhatib96@gmail.com>
2021-11-21 06:06:08 +0200

    signin page finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  CssBaseline,
+  Container,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Grid,
+  Link,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: 8,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Avatar
+            sx={{ color: "#fff", backgroundColor: "#9c27b0", margin: "8px" }}
+          >
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5" gutterBottom component="h1">
+            Sign in
+          </Typography>
+
+          <Box component="form" sx={{ mt: 1 }}>
+            <TextField
+              label="Email Address"
+              variant="outlined"
+              margin="normal"
+              id="email"
+              autoComplete="email"
+              autoFocus
+              fullWidth
+              required
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <FormControlLabel control={<Checkbox />} label="Remember me" />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          align="center"
+          sx={{ mt: 8, mb: 4 }}
+        >
+          {"Copyright © "}
+          <Link color="inherit" href="#">
+            Signin Page
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Container>
     </div>
   );
 }
